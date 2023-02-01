641648b5b2698a7229fd41dfd8e15babaafd1ec3
firefly1xx3 <eito888kun888@gmail.com>
2023-02-01 14:38:52 +0900

    first commit

---
+++ Amif-practice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Amif_practice.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
